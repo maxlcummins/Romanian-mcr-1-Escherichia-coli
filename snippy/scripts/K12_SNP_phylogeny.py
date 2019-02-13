a73e6d1eaa56d70fe8efed2824cbf877168a93f6
Max Cummins <maxcummins@MacBook-Air-8.local>
2019-02-14 08:56:31 +1100

    added R scripts

---
+++ snippy/scripts/K12_SNP_phylogeny.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from snp_phylo_utils import count_snps
+
+
+count_snps('Liv_K12.clean.filt.snp-sites.aln', 'Liv_K12.clean.filt.snp-sites.txt', 'Liv_K12.clean.filt.snp-sites_stats', 4)                                                                                       
---
+++ snippy/scripts/Liv_ST57_SNP_phylogeny.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from snp_phylo_utils import count_snps
+
+
+count_snps('Liv_ST57.clean.filt.snp-sites.aln', 'Liv_ST57.clean.filt.snp-sites.txt', 'Liv_ST57.clean.filt.snp-sites_stats', 4)                                                                                       
